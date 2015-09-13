b35bad581165a6ea966c3374bd11d9d2adcde9a1
Minh Nguyen <0912144@gmail.com>
2015-09-13 11:38:00 +0700

    first test

---
+++ STARCommon/IPlausibilityTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace STARCommon {
+    public interface IPlausibilityTest {
+
+        int GetId();
+
+        bool IsPlausible(IPlausibilityCheckContext context);
+    }
+
+    public enum PlausibilityContextTaxLiabilities {
+        Changed,
+        CurentP,
+        AsociatedFederal,
+    }
+
+    public enum PlausibilityContextPersons {
+        ChangedPerson,
+        TaxLiabilityPerson1,
+        TaxLiabilityPerson2,
+        Partner,
+    }
+
+    public enum PlausibilitySeverity {
+        Error = 1,
+        Warning = 2,
+    }
+}
---
+++ STARCommon/IPlausibilityTestFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace STARCommon {
+    public interface IPlausibilityTestFilter {
+        bool[] Accept(IPlausibilityTest plausiCheck, PlausibilitySeverity severity);
+
+        PlausibilitySeverity GetSeverity(int palusiCheckId);
+
+        PlausibilitySeverity GetSeverity(IPlausibilityTest plausiCheck);
+    }
+}
