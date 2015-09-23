e48291527702a07b8f12e0a42bcf3682c5df6b96
kleorix <0912144@gmail.com>
2015-09-23 18:45:01 +0700

    Add function to util lib

--- OOPW/Scripts/HelperScripts/UtilLib.js
+++ OOPW/Scripts/HelperScripts/UtilLib.js
@@ -2,3 +2,18 @@
     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 }
 
+
+function PaddingZero(str, max) {
+    /// <summary>
+    /// Paddings the leading zero.
+    /// </summary>
+    /// <param name="str">The string.</param>
+    /// <param name="max">The maximum.</param>
+    /// Example:
+    /// PaddingZero("3", 3);    // => "003"
+    /// PaddingZero("123", 3);  // => "123"
+    /// PaddingZero("1234", 3); // => "1234"
+    /// <returns></returns>
+    str = str.toString();
+    return str.length < max ? PaddingZero("0" + str, max) : str;
+}
